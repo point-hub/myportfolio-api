4b5c32af3a027a0fd3c955633e4797ebe2ded14c
Martien Dermawan Tanama <martiendt@gmail.com>
2026-01-28 07:15:30 +0700

    initial commit

---
+++ src/modules/_shared/data/counters.ts
@@ -0,0 +1,16 @@
+import type { ICounter } from '@/modules/counters/interface';
+
+export const getCounters = (): ICounter[] => {
+  return [
+    { name: 'roles', template: 'ROLE/<seq>', seq: 1 },
+    { name: 'owners', template: 'OWNER/<seq>', seq: 0 },
+    { name: 'banks', template: 'BANK/<seq>', seq: 0 },
+    { name: 'issuers', template: 'ISSUER/<seq>', seq: 0 },
+    { name: 'stocks', template: 'STOCK/<seq>/<yyyy><mm>', seq: 0 },
+    { name: 'savings', template: 'SAV/<seq>/<yyyy><mm>', seq: 0 },
+    { name: 'deposits', template: 'DEPO/<seq>/<yyyy><mm>', seq: 0 },
+    { name: 'bonds', template: 'BOND/<seq>/<yyyy><mm>', seq: 0 },
+    { name: 'insurances', template: 'INS/<seq>/<yyyy><mm>', seq: 0 },
+    { name: 'examples', template: 'EXAMPLE/<seq>', seq: 0 },
+  ];
+};
---
+++ src/modules/master/users/rules/update.rules.ts
@@ -0,0 +1,4 @@
+export const updateRules = {
+  name: ['sometimes', 'required', 'string', 'min:5', 'max:50'],
+  role_id: ['sometimes', 'required', 'string'],
+};
